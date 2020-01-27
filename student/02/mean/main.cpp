ccc4992f91a1d9e947d17a5f850c8e1f3443fe54
Eetu Jokinen <eetu.jokinen@tuni.fi>
2020-01-27 22:40:49 +0200

    korjattu

--- student/02/mean/main.cpp
+++ student/02/mean/main.cpp
@@ -5,8 +5,9 @@
 
 int main()
 {
-    int syote;
+    double syote;
     float mean;
+    double summa;
     std::cout << "From how many integer numbers you want to count the mean value? ";
     int maara;
     std::cin >> maara;
@@ -21,7 +22,7 @@ int main()
     }
     else {
 
-        int summa;
+
         for (int luku = 1; luku < maara + 1; ++luku) {
             std::cout << "Input " << luku <<". number: ";
             std::cin >> syote;
