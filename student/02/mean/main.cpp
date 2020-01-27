64eb664cb0a1a338e1d624112d7346deb00f4990
Eetu Jokinen <eetu.jokinen@tuni.fi>
2020-01-27 22:36:14 +0200

    testi

---
+++ student/02/mean/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+// Write here a function counting the mean value
+
+
+int main()
+{
+    int syote;
+    float mean;
+    std::cout << "From how many integer numbers you want to count the mean value? ";
+    int maara;
+    std::cin >> maara;
+    if (maara < 1){
+        std::cout << "Cannot count mean value from " << maara << " numbers" << std::endl;
+    }
+    else if (maara == 1){
+        std::cout << "Input 1. number: ";
+        std::cin >> syote;
+        mean = syote;
+        std::cout << "Mean value of the given numbers is " << mean << std::endl;
+    }
+    else {
+
+        int summa;
+        for (int luku = 1; luku < maara + 1; ++luku) {
+            std::cout << "Input " << luku <<". number: ";
+            std::cin >> syote;
+
+            summa += syote;
+
+        }
+
+        mean = summa / maara;
+        std::cout << "Mean value of the given numbers is " << mean << std::endl;
+    }
+}
