4966d442983e8a9ba4a8c847ab6a2a4ffac46031
Eetu Jokinen <eetu.jokinen@tuni.fi>
2020-01-27 23:08:55 +0200

    nolla huomioitu

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -8,9 +8,13 @@ int main()
     int no;
     cin >> no;
     int cube = no * no * no;
-    if (cube < 0 or cube / no / no != no){
+    if (no == 0){
+        cout << "The cube of 0 is 0." << endl;
+    }
+    else if (cube < 0 or cube / no / no != no){
         cout << "Error! The cube of " << no << " is not " << cube << "." << endl;
     }
+
     else{
         cout << "The cube of " << no << " is " << cube << "." << endl;
         }
