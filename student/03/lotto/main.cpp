c66b2545a3c6c28ff4fed09765afd937997433d6
Eetu Jokinen <eetu.jokinen@tuni.fi>
2020-01-28 19:33:10 +0200

    testi

---
+++ student/03/lotto/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned long int kertoma(unsigned long int luku){
+    unsigned long int summa = 1;
+    unsigned long int luku2 = luku;
+    while (luku2 > 1){
+        summa *= luku2;
+        --luku2;
+    }
+    return summa;
+}
+
+int main()
+{
+    unsigned long int odd;
+    int total;
+    int drawn;
+    cout << "Enter the total number of lottery balls: ";
+    cin >> total;
+    cout << "Enter the number of drawn balls: ";
+    cin >> drawn;
+    if (total < 1 or drawn < 1){
+        cout << "The number of balls must be a positive number." << endl;
+    }
+    else{
+        odd = kertoma(total) / (kertoma(total - drawn) * kertoma(drawn));
+        cout << "The probability of guessing all " << drawn <<  " balls correctly is 1/" << odd << endl;
+    }
+
+
+
+}
