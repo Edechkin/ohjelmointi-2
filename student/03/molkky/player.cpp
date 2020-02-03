7e0040bb754334af99541932eb47fc26eba42a50
Eetu Jokinen <eetu.jokinen@tuni.fi>
2020-02-03 23:53:52 +0200

    toimii

---
+++ student/03/molkky/player.cpp
@@ -0,0 +1,31 @@
+#include "player.hh"
+#include <iostream>
+
+
+Player::Player(std::string name)
+
+{
+    this->name = name;
+    this->points = 0;
+}
+
+int Player::get_points() {
+    return this->points;
+}
+
+std::string Player::get_name() {
+    return this->name;
+}
+
+bool Player::won() {
+    return this->points == 50;
+}
+
+void Player::add_points(int to_be_added) {
+    this->points += to_be_added;
+    if (this->points > 50) {
+        std::cout << this->name << " gets penalty points!" << std::endl;
+        this->points = 25;
+        }
+}
+
---
+++ student/03/molkky/player.hh
@@ -0,0 +1,18 @@
+#ifndef PLAYER_HH
+#define PLAYER_HH
+#include <string>
+
+class Player
+{
+public:
+    Player(std::string name);
+    std::string get_name();
+        int get_points();
+        void add_points(int to_be_added);
+        bool won();
+
+private:
+    std::string name;
+    int points;
+};
+#endif // PLAYER_HH
