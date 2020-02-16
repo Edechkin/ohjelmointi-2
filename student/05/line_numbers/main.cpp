357b2738d4fe83e53243c0075159061905f0d94c
Eetu Jokinen <eetu.jokinen@tuni.fi>
2020-02-16 17:21:42 +0200

    testi

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string syote;
+    cout << "Input file: ";
+    getline(cin, syote);
+    cout << "Output file: ";
+    string syote2;
+    getline(cin, syote2);
+
+    ifstream stiedosto(syote);
+    if (not stiedosto){
+    cout << "Error! The file " << syote << " cannot be opened." << endl;
+    return EXIT_FAILURE;
+    }
+    ofstream otiedosto(syote2);
+    int i = 0;
+    string rivi;
+    while( getline(stiedosto, rivi)){
+        otiedosto << i << " " << rivi << endl;
+        ++i;
+    }
+    stiedosto.close();
+    otiedosto.close();
+}
